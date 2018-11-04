fe05f2d629ac17f9c983c14d9d40ce133255c9bc
Vlad Coteanu <vladcoteanu1997@gmail.com>
2018-11-04 13:36:28 +0200

    Add user model. Create mongodb basic connection

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TrelloClone
+
+To set up the project you need:
+- Download and host a local server of MongoDB 
+- Run **npm install** in the root of the project
+- Run **node index.js**
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+const express = require('express');
+const mongoose = require('mongoose');
+
+const User = require('./models/user');
+const app = express();
+const router = express.Router();
+
+const db = mongoose.connection;
+mongoose.connect('mongodb://localhost:27017/trello-schema');
+db.on('error', console.error.bind(console, 'connection error:'));
+
+const server = app.listen(3000, () => console.log('server started'));
+module.exports = server;
